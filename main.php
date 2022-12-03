92adc39d1996f96f0458a2f8a17c3cb87b848a6f
asckbul <119727240+asckbul@users.noreply.github.com>
2022-12-03 13:12:21 +0500

    Add files via upload

---
+++ main.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Practice </title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    
+    <div class="flex-container">
+
+        <div class="header">     
+               <?php include 'logo.inc.php' ?>         
+               <?php include 'menu.inc.php' ?>	   
+        </div>       
+     
+        <div class="about_me">
+         
+          <h1>  <?php  echo $p  ?> </h1>
+
+            <div class="data">
+                <div class="myImg">
+                    <?php  echo '<img src="/practice/img/php.jpg">'; ?>                    
+                </div>
+
+                <div class="fullname">
+                    <p> Меня зовут 
+                    <?php echo $name, ' ', $surname . '<br>'; 
+                          echo 'город', ' ', $city; ?>                                      
+                    </p> 
+           
+                    <p> Мне
+                    <?php  echo $age;   ?>          
+                    лет </p>
+                    <p> Мы научились создавать переменные производить простые операции с ними </p>
+                    <p> Изучили язык разметки HTML </p>
+                </div>
+            </div>
+
+            <div class="knowledge">
+                                   
+                    <?php  include 'knowledge.inc.php'; ?>
+                    <?php   echo $a, ' ', $b, ' ', $c; ?> <br>
+                                       
+                    <?php
+                        $a = 10;
+                        $b = 20;
+                        $c = $a ** $b;
+                        echo $c;
+                    ?>   <br>                
+                     <?php
+                        echo $d;
+                    ?> 
+
+            </div>
+
+            <div class="article">
+                <p class="text">
+                    A chap who's supposed to stop chaps pinching things things from chaps having a chap come along and pinch something from him. <br>
+                    The Code of the Woosters (1938)
+                </p>
+            </div>
+        </div>
+
+            <?php include 'footer.inc.php' ?>
+
+    </div>
+
+
+</body>
+</html>
---
+++ menu.inc.php
@@ -0,0 +1,9 @@
+<nav>
+                <ul>
+                    <li><a href="#"> My office </a></li>
+                    <li><a href="#"> My interests </a></li>
+                    <li><a href="#"> My skills </a></li>
+                    <li><a href="#"> Links </a></li>
+                    <li><a href="#"> Contacts </a></li>
+                </ul>    
+            </nav>
